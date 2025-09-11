c06d5835a1fe797f8ab3c936222434ea11ac4dce
Anoir HMT <anouarhamtami@gmail.com>
2025-09-12 00:31:34 +0200

    feat: update hero background image

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
-import Image from "next/image"
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 export function Hero() {
-  
-  
   return (
     <section className="relative h-[50vh] sm:h-[100dvh] min-h-[300px] sm:min-h-[600px] overflow-hidden">
       {/* Background Image */}
       <Image
-        src="https://drive.google.com/uc?export=view&id=1yRxUQ1kG6J6WRiltRCEvqmzQIozmI5wo"
+        src="https://drive.google.com/uc?export=view&id=1KIcQj46EItlsooVyvdQYaZ9hl5_kZP1U"
         alt="Elarain Luxury Jewelry Collection"
         fill
         className="object-cover object-center"
@@ -27,12 +24,8 @@ export function Hero() {
 
       {/* Content */}
       <article className="relative z-10 container mx-auto px-6 h-full flex items-center">
-        <header className="max-w-4xl text-white">
-
-        </header>
+        <header className="max-w-4xl text-white"></header>
       </article>
-
-
     </section>
-  )
+  );
 }
